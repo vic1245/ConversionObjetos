8f12d0459d544f57e953ce7259156d1b85a069f0
Victor <vic12448@hotmail.com>
2023-08-26 10:33:45 -0500

    Practica de curso de java

---
+++ src/test/TestConversionObjetos.java
@@ -0,0 +1,24 @@
+package test;
+
+import domain.*;
+
+
+public class TestConversionObjetos {
+    public static void main(String[] args) {
+        Empleado empleado;
+        
+        empleado = new Escritor("Juan", 5000, TipoEscritura.CLASICO);
+//        System.out.println("empleado = " + empleado);
+
+//        System.out.println(empleado.obtenerDetalles());
+        
+        //downcasting
+        //((Escritor)empleado).getTipoEscritura();
+        Escritor escritor = (Escritor) empleado;
+        escritor.getTipoEscritura();
+        
+        //Upcasting
+        Empleado empleado2 = escritor;
+        System.out.println(empleado2.obtenerDetalles());
+    }
+}
